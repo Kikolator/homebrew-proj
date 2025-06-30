aa3573cf991dfdc84de5d4096c4532b121d75779
Nic Pijpers <nicholas@kikolator.dev>
2025-06-30 21:14:54 +0200

    proj-jumper 0.1.3: bump url & sha256

--- Formula/proj-jumper.rb
+++ Formula/proj-jumper.rb
@@ -1,23 +1,12 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class ProjJumper < Formula
   desc "Jump quickly into project directories inside $DEV_ROOT"
   homepage "https://github.com/Kikolator/proj-jumper"
-  url "https://github.com/Kikolator/proj-jumper/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "850ac146c020e38005de163b30c514758a628adfb1ef5a489f8afa65751319c1"
+  url "https://github.com/Kikolator/proj-jumper/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "0b600460b07225e18ad417706589d4eeb9d9a6c41e2014e0633c83777b8ea8dc"
   license "MIT"
 
-  # depends_on "cmake" => :build
-
-  # Additional dependency
-  # resource "" do
-  #   url ""
-  #   sha256 ""
-  # end
-
   def install
-    pkgshare.install Dir["*"]  # keep repo layout intact
+    pkgshare.install Dir["*"]
   end
 
   def caveats
@@ -28,19 +17,16 @@ class ProjJumper < Formula
         sed -i '' 's/^plugins=(/&proj-jumper /' ~/.zshrc
 
       Then restart your shell or `source ~/.zshrc`.
+
+      👉  **One‑time configuration**
+
+          export PROJ_DEV_ROOT=/path/to/projects        # manual
+          #‑‑OR‑‑
+          proj-config /path/to/projects                 # auto‑writes the line
     EOS
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test proj-jumper`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
     assert_match "proj", shell_output("zsh -c 'source #{pkgshare}/proj.plugin.zsh && which proj'")
   end
 end
